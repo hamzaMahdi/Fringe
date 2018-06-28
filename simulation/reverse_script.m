c435952ab45a43dd4aa37daf9802069d29e65c1a
hamzaMahdi <hamzamahdi96@gmail.com>
2018-06-28 13:39:53 -0400

    added automatic detection of local maxima and janky recreation of wrapped phase

--- simulation/reverse_script.m
+++ simulation/reverse_script.m
@@ -4,18 +4,35 @@
 imageSizeX = 640;
 imageSizeY = 480;
 [columnsInImage, rowsInImage] = meshgrid(1:imageSizeX, 1:imageSizeY);
+phaseImage = zeros(size(columnsInImage));
+phaseImage(100,200) = 1;phaseImage(400,100) = 1;%manually create two point sources
+%the code below is to find the coordinates of the point created manually
+hLocalMax = vision.LocalMaximaFinder;
+      hLocalMax.MaximumNumLocalMaxima = 2;%TODO: make this automatic int the future
+      hLocalMax.NeighborhoodSize = [5 5];
+      hLocalMax.Threshold = 0.7*max(max(phaseImage));
+
+      location = step(hLocalMax, phaseImage)
 % Next create the circle in the image.
+
+%initialize some values
+innerRadius = 20;
+outerRadius = 40;
+ringPixels = zeros(size(columnsInImage));
+for i = 1:size(location,1)%adapts to the number of local maxima
+    centerX = location(i,1);
+    centerY = location(i,2);
+    array2D = (int16(rowsInImage) - int16(centerY)).^2 ...
+    + (int16(columnsInImage) - int16(centerX)).^2;
+    ringPixels = ringPixels+ (array2D >= innerRadius.^2 & array2D <= outerRadius.^2);
+end
 centerX = 320;
 centerY = 240;
-innerRadius = 40;
-outerRadius = 60;
+
 
 innerRadius1 = 80;
 outerRadius1 = 100;
-array2D = (rowsInImage - centerY).^2 ...
-    + (columnsInImage - centerX).^2;
-ringPixels = (array2D >= innerRadius.^2 & array2D <= outerRadius.^2) ...
-+ (array2D >= innerRadius1.^2 & array2D <= outerRadius1.^2);
+
 % ringPixels is a 2D "logical" array.
 % Now, display it.
 ringPixels = ringPixels*256;%increase contrast
