aa5c4c9398560ea99181828facf7768e6970a1c0
hamzaMahdi <hamzamahdi96@gmail.com>
2018-07-02 01:59:17 -0400

    added more points, variable but not gaussian yet

--- simulation/reverse_script.m
+++ simulation/reverse_script.m
@@ -5,37 +5,41 @@ imageSizeX = 640;
 imageSizeY = 480;
 [columnsInImage, rowsInImage] = meshgrid(1:imageSizeX, 1:imageSizeY);
 phaseImage = zeros(size(columnsInImage));
-phaseImage(100,200) = 10;phaseImage(400,100) = 20;%manually create two point sources
+phaseImage(100,200) = 30;phaseImage(400,100) = 20;%manually create two point sources
+phaseImage(300,400) = 10;
 %the code below is to find the coordinates of the point created manually
 hLocalMax = vision.LocalMaximaFinder;
-      hLocalMax.MaximumNumLocalMaxima = 2;%TODO: make this automatic int the future
+      hLocalMax.MaximumNumLocalMaxima = 3;%TODO: make this automatic int the future
       hLocalMax.NeighborhoodSize = [5 5];
-      hLocalMax.Threshold = 0.4*max(max(phaseImage));
+      hLocalMax.Threshold = 0.3*max(max(phaseImage));
 
       location = step(hLocalMax, phaseImage)
 % Next create the circle in the image.
 
 %initialize some values
-innerRadius = 20;
-outerRadius = 40;
 ringPixels = zeros(size(columnsInImage));
 for i = 1:size(location,1)%adapts to the number of local maxima
-    innerRadius = 20;
-    outerRadius = 40;
+    innerRadius = 35;
+    outerRadius = 45;
     centerX = location(i,1);
     centerY = location(i,2);
     array2D = (int16(rowsInImage) - int16(centerY)).^2 ...
     + (int16(columnsInImage) - int16(centerX)).^2;
     k = 1;
-    while 1
+    while 1  
         ringPixels = ringPixels+ (array2D >= innerRadius.^2 & array2D <= outerRadius.^2);
         imagesc(ringPixels)
         if(phaseImage(centerY,centerX)-2*pi*k<=0)
             break;
         end
         innerRadius = outerRadius+20;
-        outerRadius = outerRadius+40;
+        outerRadius = outerRadius+40-k*4;
         k = k+1;
     end
 end
-imagesc(ringPixels);colormap gray;
+imagesc(ringPixels);colormap(gray(256))
+title('final wrapped phase information (reverse process)');
+figure;imagesc(phaseImage);
+hold on;plot(200,100,'ow');plot(100,400,'ow');plot(400,300,'ow');
+title('original unwrapped phase information with point sources highlighted')
+colormap(gray(256))
