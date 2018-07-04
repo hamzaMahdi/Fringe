779ffdd8c1d462731d062876d8dfd4195bd3b84a
hamzaMahdi <hamzamahdi96@gmail.com>
2018-07-04 13:06:18 -0400

    script now unwraps the simulated wrapped phase to compare to original unwrapped input

--- simulation/reverse_script.m
+++ simulation/reverse_script.m
@@ -43,3 +43,20 @@ figure;imagesc(phaseImage);
 hold on;plot(200,100,'ow');plot(100,400,'ow');plot(400,300,'ow');
 title('original unwrapped phase information with point sources highlighted')
 colormap(gray(256))
+%%
+
+unwrapped = do_all_rows(ringPixels*255);
+BW = imregionalmax(unwrapped);
+%figure;imshow(BW);
+unwrapped1 = unwrapped .* BW;
+unwrapped1 = imhmax(unwrapped1,max(max(unwrapped1)-0.001));%this thresholds a second time to clear the image completely
+
+CPL =0.01;%10 mm
+alpha = 36;%degrees
+WL = 650*10^-9;%wave length
+RI = unwrapped1 * WL/(cos(alpha/2))/CPL;
+figure;
+a = subplot(3,2,[1 ; 4]);imagesc(ringPixels);title("original");colormap(a, gray);
+subplot(3,2,5);
+mesh(unwrapped);title("mathematical unwrapping");colormap default;
+subplot(3,2,6);mesh(RI);title("point source refractive index modification");
